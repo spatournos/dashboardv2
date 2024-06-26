d4666d99d5979716369b929e99dd3e22967cc5a6
spatournos <kavadas.georgios@nbg.gr>
2024-06-26 08:39:17 +0300

    Line and Geography Charts added

--- src/App.js
+++ src/App.js
@@ -13,9 +13,9 @@ import Calendar from "./scenes/calendar";
 import Form from "./scenes/form";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
-// import Line from "./scenes/line";
 import Pie from "./scenes/pie";
-// import Geography from "./scenes/geography";
+import Line from "./scenes/line";
+import Geography from "./scenes/geography";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -38,8 +38,8 @@ function App() {
               <Route path="/faq" element={<FAQ />} />
               <Route path="/bar" element={<Bar />} />
               <Route path="/pie" element={<Pie />} />
-              {/* <Route path="/line" element={<Line />} /> */}
-              {/* <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/line" element={<Line />} />
+              <Route path="/geography" element={<Geography />} />
             </Routes>
         </main> 
       </div>
